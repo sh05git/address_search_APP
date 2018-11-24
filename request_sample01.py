d8ecd576cc6e39091775e319148eb15da0cb640d
shogo <bttw.1011@gmail.com>
2018-11-24 17:11:49 +0900

    JSONコードから岩手県八幡平市大更の郵便番号を取得できた。

--- request_sample01.py
+++ request_sample01.py
@@ -2,8 +2,8 @@ import requests
 
 # response = requests.get(url="http://hachimantai.spartacamp.jp/")
 
-response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
+response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=1760014")
 print(response)
 
-response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=8150074")
-print(response)
+# response = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=8150074")
+# print(response)
--- test_address_seacher.py
+++ test_address_seacher.py
@@ -1,9 +1,17 @@
 import unittest
+import requests
 
 
 class AddressSeacher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111"
+        response = requests.get(url)
+        response_dict = response.json()
+        都道府県 = response_dict["results"][0]["address1"]
+        市区町村 = response_dict["results"][0]["address2"]
+        町域 = response_dict["results"][0]["address3"]
+        return f"{都道府県}{市区町村}{町域}"
+
 
 class TestAddressSeacher(unittest.TestCase):
     def test_岩手県八幡平市大更の地名を郵便番号から取得できる(self):
@@ -18,7 +26,8 @@ class TestAddressSeacher(unittest.TestCase):
 
         actual = address_sercher.search(postal_code="1760014")
 
-        self.assertEqual("岩手県八幡平市大更", actual)
+        self.assertEqual("東京都練馬区豊玉南", actual)
+
 
 if __name__ == "__main__":
     unittest.main()
